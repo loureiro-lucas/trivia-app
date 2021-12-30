bc0e185fa487716e30bf2c803021347b903db42f
loureiro-lucas <lucas-loureiro@outlook.com>
2021-12-30 15:20:37 -0300

    material ui installed

--- trivia-app/src/index.js
+++ trivia-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './index.css';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
